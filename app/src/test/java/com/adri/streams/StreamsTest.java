90124bfc61b84c004b6b9480bf879967da3375f3
adriferracuti <1831360+adriferracuti@users.noreply.github.com>
2021-04-30 16:03:52 +0200

    Converted to gradle project, added streams tests

---
+++ app/src/test/java/com/adri/streams/StreamsTest.java
@@ -0,0 +1,37 @@
+package com.adri.streams;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StreamsTest {
+
+    @Test
+    void bitSetAsStream() {
+        var bitSet = new BitSet();
+        bitSet.set(0);
+
+        var foo = Arrays.stream(
+                bitSet.toLongArray()
+        );
+
+        bitSet.set(3);
+
+        assertArrayEquals(new long[]{1}, foo.toArray());
+    }
+
+    @Test
+    void mutatedListAsStream() {
+        var foo = new ArrayList<>(List.of(1, 2, 3));
+        var fooStream = foo.stream();
+        foo.add(4);
+        assertEquals(List.of(1, 2, 3, 4), fooStream.collect(Collectors.toList()));
+    }
+}
